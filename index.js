52e210165689ca247a2c7936b8a3b156cce0b13d
Shree Murugesan <shree.murugesan311@gmail.com>
2021-01-09 13:23:01 -0500

    updated mongodb url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 // app.use(express.static('client/build'));
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
