47e0136981ee323a26c1bb8f3a83e23676967317
Shree Murugesan <shree.murugesan311@gmail.com>
2021-01-09 12:46:23 -0500

    config backend

--- server/index.js
+++ server/index.js
@@ -2,22 +2,27 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import MONGODB_URI from './config/index.js';
 
 import bookRoutes from './routes/books.js';
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 4000;
-const CONNECTION_URL = 'mongodb+srv://shreemur:DMS93fA0bDsZougg@cluster0.wvg7k.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/books', bookRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello to BookShelf API');
+});
+
 // app.use(express.static('client/build'));
 
-mongoose.connect(process.env.MONGODB_URI || MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
